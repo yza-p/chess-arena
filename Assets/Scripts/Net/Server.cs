2dbc3451db4306ded735dfd8b8880de57b658774
Yza Paquiao <93480398+yza-p@users.noreply.github.com>
2024-04-23 14:31:31 -0700

    Added server functions

--- Assets/Scripts/Net/Server.cs
+++ Assets/Scripts/Net/Server.cs
@@ -61,5 +61,79 @@ public class Server : MonoBehaviour
 
     public void Update()
     {
+        if (!isActive)
+            return;
+
+        driver.ScheduleUpdate().Complete();
+
+        CleanupConnections();
+        AcceptNewConnections();
+        UpdateMessagePump();
+    }
+
+
+    private void CleanupConnections()
+    {
+        // Cleanup old references to old connections
+        for (int i = 0; i < connections.Length; i++)
+        {
+            if (!connections[i].IsCreated)
+            {
+                connections.RemoveAtSwapBack(i);
+                --i;
+            }
+        }
+    }
+
+    private void AcceptNewConnections()
+    {
+        NetworkConnection c;
+        while ((c = driver.Accept()) != default(NetworkConnection))
+        {
+            connections.Add(c);
+        }
+    }
+
+    private void UpdateMessagePump()
+    {
+        DataStreamReader stream;
+        for (int i = 0; i < connections.Length; ++i)
+        {
+            NetworkEvent.Type cmd;
+            while ((cmd = driver.PopEventForConnection(connections[i], out stream)) != NetworkEvent.Type.Empty)
+            {
+                if (cmd == NetworkEvent.Type.Data)
+                {
+                    // NetUtility.OnData(stream, connections[i], this);
+                }
+                else if (cmd == NetworkEvent.Type.Disconnect)
+                {
+                    Debug.Log("Client disconnected from server");
+                    connections[i] = default(NetworkConnection);
+                    connectionDropped?.Invoke();
+                    Shutdown();
+                }
+            }
+        }
+    }
+
+
+    public void SendToClient(NetworkConnection connection, NetMessage msg)
+    {
+        DataStreamWriter writer;
+        driver.BeginSend(connection, out writer);
+        // msg.Serialize(ref writer);
+        driver.EndSend(writer);
+    }
+    public void Broadcast(NetMessage msg)
+    {
+        for (int i = 0; i < connections.Length; i++)
+        {
+            if (connections[i].IsCreated)
+            {
+                // Debug.Log($"Sending {msg.Code} to : {connections[i]}");
+                SendToClient(connections[i], msg);
+            }
+        }
     }
 }
