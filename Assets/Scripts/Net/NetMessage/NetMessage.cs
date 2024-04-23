354140debfc77224da9c5de3edcf118a977741e8
Yza Paquiao <93480398+yza-p@users.noreply.github.com>
2024-04-23 15:02:37 -0700

    Placeholder functions for NetMessage

--- Assets/Scripts/Net/NetMessage/NetMessage.cs
+++ Assets/Scripts/Net/NetMessage/NetMessage.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
+
+public enum OpCode
+{
+    KEEP_ALIVE = 0,
+    WELCOME = 1,
+    START_GAME = 2,
+    MAKE_MOVE = 4,
+    REMATCH = 5
+}
 
 public class NetMessage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public OpCode Code { get; set; }
+
+    public virtual void Serialize(ref DataStreamWriter writer)
     {
-        
+        writer.WriteByte((byte)Code);
+    }
+    public virtual void Deserialize(ref DataStreamReader reader)
+    {
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public virtual void ReceivedOnClient()
     {
-        
+
     }
+    public virtual void ReceivedOnServer()
+    {
+
+    }
+
 }
