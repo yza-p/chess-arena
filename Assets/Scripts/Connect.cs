549a6da8e4ea99671ecf3c52f3eb95377bf5a94e
Yza Paquiao <93480398+yza-p@users.noreply.github.com>
2024-04-24 09:39:26 -0700

    Fixed minor bug in Connect screen, more reliable loopback connection

--- Assets/Scripts/Connect.cs
+++ Assets/Scripts/Connect.cs
@@ -19,22 +19,22 @@ public class Connect : MonoBehaviour
 
     public void OnButtonPlay(int sceneID)
     {
-        server.Init(8007);
-        client.Init("127.0.0.1", 8007);
         SceneManager.LoadScene(sceneID);
+        /*server.Init(8007);
+        client.Init("127.0.0.1", 8007);*/
     }
 
     public void OnCreateRoom()
     {
-        server.Init(8007);
-        client.Init("127.0.0.1", 8007);
+        server.Init(8230);
+        client.Init("127.0.0.1", 8230);
         loadingOverlay.SetActive(true);
         //Debug.Log("OnCreateRoom");
     }
 
     public void OnJoinRoom()
     {
-        client.Init(addressInput.text, 8007);
+        client.Init(addressInput.text, 8230);
     }
 
     public void OnCancelConnect()
